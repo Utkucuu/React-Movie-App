3c8a7a7e3b43f9f846398c816094f456a35e89d5
utku <bayraktarogluutku@gmail.com>
2023-11-03 04:43:45 +0300

    pagenav css correction

--- src/components/movies/leftPanel/PageNav.js
+++ src/components/movies/leftPanel/PageNav.js
@@ -2,7 +2,7 @@ import TopPageNavItem from "./PageNavItem";
 function PageNav() {
   console.log("MOVIES PAGENAV rendered");
   return (
-    <div className="my-5 flex items-center justify-between divide-x rounded-xl bg-gradient-to-b from-cyan-400 to-sky-900 dark:from-slate-900 dark:to-sky-900 xs:p-2 md:p-4 lg:my-10">
+    <div className="my-5 flex items-center justify-between divide-x rounded-xl bg-gradient-to-b from-cyan-400 to-sky-900 py-1 dark:from-slate-900 dark:to-sky-900 xs:p-2 md:p-4 lg:my-10">
       {/* <TopPageNavItem to={""}>Tüm Filmler</TopPageNavItem> */}
       <TopPageNavItem to={""}>Popüler Filmler</TopPageNavItem>
       <TopPageNavItem to={"trend"}>Trend Filmler</TopPageNavItem>
